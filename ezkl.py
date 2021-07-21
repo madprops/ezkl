f5694afeb517232c9d0ede2e06a071f8cc3b8e41
Auric Vente <mprops@protonmail.com>
2021-07-21 08:20:56 -0600

    mods

--- ezkl.py
+++ ezkl.py
@@ -3,25 +3,6 @@ import sys
 from pathlib import Path
 from difflib import SequenceMatcher
 
-args = [x for x in sys.argv[1:] if not x.startswith("-")]
-
-if len(args) != 2:
-  exit(0)
-
-mode = args[0]
-path = args[1]
-pwd = os.getenv("PWD").rstrip("/")
-
-thispath = Path(__file__).parent.resolve()
-filepath = Path(thispath) / Path("paths.txt")
-filepath.touch(exist_ok=True)
-file = open(filepath, "r+")
-paths = file.read().split("\n")
-file.close()
-
-min_accuracy = 0.6
-max_paths = 300
-
 def filterpath(filter):
   pths = [filter]
 
@@ -106,16 +87,39 @@ def updatefile(paths):
 def similar(a, b):
   return SequenceMatcher(None, a, b).ratio()
 
+def cleanpath(path):
+  return path.rstrip("/")
+
 if __name__ == "__main__":
+  args = [x for x in sys.argv[1:] if not x.startswith("-")]
+
+  if len(args) != 2:
+    exit(0)
+
+  mode = args[0]
+  keyword = args[1]
+  pwd = cleanpath(os.getenv("PWD"))
+  min_accuracy = 0.6
+  max_paths = 300
+
+  thispath = Path(__file__).parent.resolve()
+  filepath = Path(thispath) / Path("paths.txt")
+  filepath.touch(exist_ok=True)
+  file = open(filepath, "r+")
+  paths = file.read().split("\n")
+  file.close()
+
   if mode == "remember":
     updatefile(filterpath(pwd))
   elif mode == "forget":
-    updatefile(forgetpath(path))
+    updatefile(forgetpath(keyword))
   elif mode == "jump":
-    p = path if path.startswith("/") \
-      else findpath(path)
-    if len(p) == 0:
-      p = checkhome(path)
-    if len(p) > 0:
-      updatefile(filterpath(p))
-      print(p)
+    if keyword.startswith("/"):
+      path = cleanpath(keyword) 
+    else:
+      path = findpath(keyword)
+    if len(path) == 0:
+      path = checkhome(keyword)
+    if len(path) > 0:
+      updatefile(filterpath(path))
+      print(path)
