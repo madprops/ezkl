6008055a4c9e2510b642f3873ed73fb2be1e3e4f
Auric Vente <mprops@protonmail.com>
2021-07-22 05:39:05 -0600

    infomod and getpaths

--- ezkl.py
+++ ezkl.py
@@ -81,12 +81,26 @@ def cleanpath(path):
   return path.rstrip("/")
 
 def showinfo():
-  info = """\nJump around directories. For instance 'z music'
+  info = f"""\nezkl is installed and ready to use
+---------------------------------------------
+Jump around directories. For instance 'z music'
 Directories get remembered by using cd normally
 Paths are saved in ezkl/paths.txt
 If you don't need this anymore remove, ezkl from ~/.bashrc
-Remember to restart Bash for changes to apply\n"""
+Remember to restart Bash for changes to apply
+---------------------------------------------
+paths.txt has {len(paths)} paths saved\n"""
   print(info)
+
+def getpaths():
+  global paths
+  global filepath
+  thispath = Path(__file__).parent.resolve()
+  filepath = Path(thispath) / Path("paths.txt")
+  filepath.touch(exist_ok=True)
+  file = open(filepath, "r")
+  paths = file.read().split("\n")
+  file.close()
 
 if __name__ == "__main__":
   args = [x for x in argv[1:] if not x.startswith("-")]
@@ -94,20 +108,16 @@ if __name__ == "__main__":
   if len(args) >= 1:
     mode = args[0]
     if mode == "info":
+      getpaths()
       showinfo()
       exit(0)
 
   if len(args) < 2:
     exit(0)
 
+  getpaths()
   keyword = args[1]
   pwd = cleanpath(getenv("PWD"))
-  thispath = Path(__file__).parent.resolve()
-  filepath = Path(thispath) / Path("paths.txt")
-  filepath.touch(exist_ok=True)
-  file = open(filepath, "r")
-  paths = file.read().split("\n")
-  file.close()
 
   if mode == "remember":
     updatefile(filterpath(pwd))
