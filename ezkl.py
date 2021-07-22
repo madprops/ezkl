914a3ed366758fe88a523a8491f38dd5d7febeb9
Auric Vente <mprops@protonmail.com>
2021-07-22 07:45:48 -0600

    mainmod2

--- ezkl.py
+++ ezkl.py
@@ -33,6 +33,36 @@ def main():
       updatefile(filterpath(path))
       print(path)
 
+# Get arguments. Might exit here
+def getargs():
+  global mode
+  global keyword
+
+  args = [x for x in argv[1:] if not x.startswith("-")]
+  mode = args[0] if len(args) > 0 else ""
+  keyword = args[1] if len(args) > 1 else ""
+
+  if mode not in ["remember", "forget", "jump", "info"]:
+    exit(0)
+
+  if mode != "info" and keyword == "":
+    exit(0)
+
+# Read the paths file plus other paths
+def getpaths():
+  global paths
+  global filepath
+  global pwd
+  thispath = Path(__file__).parent.resolve()
+  filepath = Path(thispath) / Path("paths.txt")
+  filepath.touch(exist_ok=True)
+  file = open(filepath, "r")
+  paths = file.read().split("\n")
+  paths = list(map(str.strip, paths))
+  paths = list(filter(None, paths))
+  file.close()
+  pwd = cleanpath(getenv("PWD"))
+
 # Put path in first line
 def filterpath(path):
   pths = [path]
@@ -131,36 +161,6 @@ Remember to restart Bash for changes to apply
 Minimum accuracy is set to {min_accuracy}
 paths.txt has {len(paths)}/{max_paths} paths saved\n"""
   print(info)
-
-# Get arguments. Might exit here
-def getargs():
-  global mode
-  global keyword
-
-  args = [x for x in argv[1:] if not x.startswith("-")]
-  mode = args[0] if len(args) > 0 else ""
-  keyword = args[1] if len(args) > 1 else ""
-
-  if mode not in ["remember", "forget", "jump", "info"]:
-    exit(0)
-
-  if mode != "info" and keyword == "":
-    exit(0)
-
-# Read the paths file plus other paths
-def getpaths():
-  global paths
-  global filepath
-  global pwd
-  thispath = Path(__file__).parent.resolve()
-  filepath = Path(thispath) / Path("paths.txt")
-  filepath.touch(exist_ok=True)
-  file = open(filepath, "r")
-  paths = file.read().split("\n")
-  paths = list(map(str.strip, paths))
-  paths = list(filter(None, paths))
-  file.close()
-  pwd = cleanpath(getenv("PWD"))
 
 # Program starts here
 if __name__ == "__main__": main()
