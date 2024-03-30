3b6ab3781cb9218e8af86a508907c16ac8cd8ef4
Auric Vente <mprops@protonmail.com>
2024-03-30 06:01:17 -0600

    Mods

--- ezkl.py
+++ ezkl.py
@@ -1,232 +1,182 @@
 #!/usr/bin/env python
-
-# Imports
-import re
 from sys import argv, stderr
 from os import getenv
-from typing import List, Match
+from typing import List
 from pathlib import Path
 from subprocess import Popen, PIPE
 
-# Globals
-mode: str
-keyw: str
-paths: List[str]
-filepath: Path
-pwd: str
-
-# Match class
 class Match:
-  def __init__(self, path) -> None:
+  def __init__(self, path: str) -> None:
     self.path = path
     self.level = 0
 
-# List of matches
 class MatchList:
   items: List[Match]
 
   def __init__(self) -> None:
     self.items = []
 
-  # Add an item
   def add(self, match: Match) -> None:
     self.items.append(match)
 
-  # Append two lists
-  def join(self, matches: "MatchList") -> None:
-    for item in matches.items:
-      if item not in self.items:
-        self.items.append(item)
-
-  # Get a slice of matches
-  def slice(self, max: int) -> List[Match]:
-    paths: List[Match] = []
-
-    for m in self.items[0:max]:
-      paths.append(m)
-
-    return paths
-
-  # Get first item
-  def first(self) -> str:
+  def first(self) -> Match:
     return self.items[0]
 
-  # Get number of matches
-  def len(self) -> int:
-    return len(self.items)
-
-  # Check if list has item
-  def has(self, match: str) -> bool:
-    for m in self.items:
-      if m == match:
-        return True
-    return False
-
-  # Check if there are no items
   def empty(self) -> bool:
     return len(self.items) == 0
-  
-  # Remove item
-  def remove(self, value):
-    self.items.remove(value)
-  
-  # Sort paths naturally
-  def sort(self, keyword):
+
+  def sort(self, keyword: str) -> None:
     kw = keyword.lower()
 
     for item in self.items:
       parts = item.path.split("/")
       parts.reverse()
       level = 0
-      
+
       for i, p in enumerate(parts):
         pl = p.lower()
         if pl == kw:
           level += 10 + (len(parts) - i)
         elif kw in pl:
           level += 5 + (len(parts) - i)
-      
+
       item.level = level
-    
+
     self.items.sort(key=lambda x: x.path, reverse=False)
     self.items.sort(key=lambda x: x.level, reverse=True)
-  
-  # Get a list of paths
+
   def paths(self) -> List[str]:
     return [x.path for x in self.items]
 
-# Get arguments. Might exit here
-def get_args() -> None:
-  global mode
-  global keyw
+class Args:
+  mode = ""
+  keyword = ""
 
-  args = argv[1:]
-  mode = args[0] if len(args) > 0 else ""
+  @staticmethod
+  def get_args() -> None:
+    args = argv[1:]
+    Args.mode = args[0] if len(args) > 0 else ""
 
-  if mode == "":
-    info("remember = remember path")
-    info("jump arg = jump to path")
-    info("forget = forget path")
-    info("list = list paths")
-    info("clear = forget all paths")
-    info("----")
-    info("Create an alias in your shell for 'cd $(ezkl jump something)'")
-    exit(0)
+    if Args.mode == "":
+      info("remember = remember path")
+      info("jump arg = jump to path")
+      info("forget = forget path")
+      info("list = list paths")
+      info("clear = forget all paths")
+      info("----")
+      info("Create an alias in your shell for 'cd $(ezkl jump something)'")
+      exit(0)
 
-  if mode not in ["remember", "forget", "jump", "list", "clear"]:
-    exit(1)
+    if Args.mode not in ["remember", "forget", "jump", "list", "clear"]:
+      exit(1)
 
-  keyw = " ".join(args[1:]) if len(args) > 1 else ""
+    Args.keyword = " ".join(args[1:]) if len(args) > 1 else ""
 
-  if mode == "jump" and keyw == "":
-    exit(0)
+    if Args.mode == "jump" and Args.keyword == "":
+      exit(0)
 
-# Get current working directory
-def get_pwd() -> None:
-  global pwd
-  pwd = clean_path(str(getenv("PWD")))
+class Paths:
+  pwd = ""
+  filepath = Path("")
+  paths: List[str] = []
 
-# Read the paths file plus other paths
-def get_paths() -> None:
-  global paths
-  global filepath
+  @staticmethod
+  def get_pwd() -> None:
+    Paths.pwd = Paths.clean_path(str(getenv("PWD")))
 
-  configdir = Path("~/.config/ezkl").expanduser()
+  @staticmethod
+  def get_paths() -> None:
+    configdir = Path("~/.config/ezkl").expanduser()
 
-  if not configdir.exists():
-    configdir.mkdir(parents=True)
+    if not configdir.exists():
+      configdir.mkdir(parents=True)
 
-  filepath = configdir / Path("paths.txt")
-  filepath.touch(exist_ok=True)
+    Paths.filepath = configdir / Path("paths.txt")
+    Paths.filepath.touch(exist_ok=True)
 
-  file = open(filepath, "r")
-  paths = file.read().split("\n")
-  paths = list(map(str.strip, paths))
-  paths = list(filter(None, paths))
-  file.close()
+    file = open(Paths.filepath, "r")
+    Paths.paths = file.read().split("\n")
+    Paths.paths = list(map(str.strip, Paths.paths))
+    Paths.paths = list(filter(None, Paths.paths))
+    file.close()
 
-# Put path in first line
-def filter_path(path: str) -> None:
-  global paths
-  pths = [path]
+  @staticmethod
+  def filter_path(path: str) -> None:
+    paths = [path]
 
-  for p in paths:
-    if p == path:
-      continue
-    pths.append(p)
+    for p in Paths.paths:
+      if p == path:
+        continue
 
-  paths = pths
+      paths.append(p)
 
-# Remember path
-def remember_path(path: str) -> None:
-  if path in paths:
-    info("Path already remembered")
-  else:
-    filter_path(path)
+    Paths.paths = paths
+
+  @staticmethod
+  def remember_path(path: str) -> None:
+    if path in Paths.paths:
+      info("Path already remembered")
+    else:
+      Paths.filter_path(path)
+      update_file()
+      info("Path remembered")
+
+  @staticmethod
+  def forget_path(path: str) -> None:
+    if path not in Paths.paths:
+      info("Path is not remembered")
+      return
+
+    paths: List[str] = []
+
+    for p in Paths.paths:
+      if p == path:
+        continue
+
+      paths.append(p)
+
+    Paths.paths = paths
+
     update_file()
-    info("Path remembered")
+    info("Path forgotten")
 
-# Remove path and subdirs
-def forget_path(path: str) -> None:
-  global paths
-  path_included = False
+  @staticmethod
+  def clean_path(path: str) -> str:
+    return path.rstrip("/")
 
-  if path not in paths:
-    info("Path is not remembered")
-    return
+  @staticmethod
+  def list_paths() -> None:
+    for path in Paths.paths:
+      print(path)
 
-  pths: List[str] = []
+  @staticmethod
+  def clear_paths() -> None:
+    try:
+      ans = input("Forget all paths? (y/N): ")
 
-  for p in paths:
-    if p == path:
-      continue
+      if ans == "y":
+        Paths.paths = []
+        update_file()
+        info("Paths cleared")
+    except KeyboardInterrupt:
+      pass
 
-    pths.append(p)
-
-  paths = pths
-
-  update_file()
-  info("Path forgotten")
-
-# Find matching paths
 def get_matches(keyword: str) -> MatchList:
   matches = MatchList()
   lowkeyword = keyword.lower()
 
-  for path in paths:
+  for path in Paths.paths:
     if lowkeyword in path.lower():
       matches.add(Match(path))
-  
+
   matches.sort(keyword)
   return matches
 
-# Write paths to file
 def update_file() -> None:
-  file = open(filepath, "w")
-  file.write("\n".join(paths).strip())
+  file = open(Paths.filepath, "w")
+  file.write("\n".join(Paths.paths).strip())
   file.close()
 
-# Remove unecessary characters
-def clean_path(path: str) -> str:
-  return path.rstrip("/")
-
-# List the paths file
-def list_paths() -> None:
-  for path in paths:
-    print(path)
-
-# Clear the paths file
-def clear_paths() -> None:
-  global paths
-
-  ans = input("Forget all paths? (y/n): ")
-
-  if ans == "y":
-    paths = []
-    update_file()
-    info("Paths cleared")
-
-# Select a match with rofi
 def select_match(matches: MatchList) -> None:
   cmd = 'rofi -dmenu -markup-rows -p "Select Match (Alt+1 Forget)"'
   proc = Popen(cmd, stdout=PIPE, stdin=PIPE, shell=True, text=True)
@@ -235,19 +185,18 @@ def select_match(matches: MatchList) -> None:
   if ans != "":
     code = proc.returncode
     if code == 10:
-      forget_path(ans)
+      Paths.forget_path(ans)
       return
     else:
       print(ans)
 
-# Main jump function
 def jump() -> None:
-  if len(paths) == 0:
+  if len(Paths.paths) == 0:
     info("No paths remembered yet")
     exit(1)
 
-  matches = get_matches(keyw)
-  
+  matches = get_matches(Args.keyword)
+
   if not matches.empty():
     if len(matches.items) > 1:
       select_match(matches)
@@ -256,30 +205,28 @@ def jump() -> None:
   else:
     info("No path found")
 
-# Show a message
 def info(msg: str, title: str = "ezkl") -> None:
   print(f"{title}: {msg}", file=stderr)
 
-# Main function
 def main() -> None:
-  get_pwd()
-  get_args()
-  get_paths()
+  Paths.get_pwd()
+  Args.get_args()
+  Paths.get_paths()
 
-  if mode == "remember":
-    remember_path(pwd)
+  if Args.mode == "remember":
+    Paths.remember_path(Paths.pwd)
 
-  elif mode == "forget":
-    forget_path(keyw or pwd)
+  elif Args.mode == "forget":
+    Paths.forget_path(Args.keyword or Paths.pwd)
 
-  elif mode == "list":
-    list_paths()
+  elif Args.mode == "list":
+    Paths.list_paths()
 
-  elif mode == "clear":
-    clear_paths()
+  elif Args.mode == "clear":
+    Paths.clear_paths()
 
-  elif mode == "jump":
+  elif Args.mode == "jump":
     jump()
-  
-# Program starts here
-if __name__ == "__main__": main()
+
+if __name__ == "__main__":
+  main()
